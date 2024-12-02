0a1afa66308675982e3db1cfac7315ac50a275ea
jungmayo <wlgus5946@gmail.com>
2024-12-02 18:20:44 +0900

    drive view + insert

--- src/main/java/BackAnt/entity/FileEntity.java
+++ src/main/java/BackAnt/entity/FileEntity.java
@@ -3,6 +3,9 @@ package BackAnt.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 /*
     날 짜 : 2024/11/28(목)
@@ -22,22 +25,30 @@ public class FileEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int fid;
+    private int fileId;
 
     private String oName; // 파일 기존 이름
     private String sName; // 파일 저장된 이름
     private String filePath; // 파일 경로
     private long fileSize; // 파일 크기
 
-
     @ColumnDefault("0") // 디폴트 값 - 삭제되지 않음
     private int isDeleted; //
 
-//    @ManyToOne
-//    @JoinColumn(name = "folder_id")
-//    private Folder folder;  // 파일이 속한 폴더
-//    private LocalDateTime createdAt;
-//    private LocalDateTime updatedAt;
+    @ColumnDefault("0") // 디폴트 값 - 즐찾되지 않음
+    private int isFavorite;
+
+    @ColumnDefault("0") // (0이면 공유중아님, 1이면 공유중)
+    private int shareType;
+
+    private String folderId; // 본인의 폴더아이디에 담긴 파일
+
+    private String maker; // 등록한 사람
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
 
 
 }
---
+++ src/main/java/BackAnt/service/DriveFolderService.java
@@ -0,0 +1,55 @@
+package BackAnt.service;
+
+import BackAnt.document.page.drive.DriveFolderDocument;
+import BackAnt.dto.drive.DriveNewFolderInsertDTO;
+import BackAnt.dto.drive.MyDriveViewDTO;
+import BackAnt.repository.mongoDB.drive.DriveFolderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Log4j2
+@Service
+public class DriveFolderService {
+
+    private final DriveFolderRepository driveFolderRepository;
+    private final ModelMapper modelMapper;
+
+    public DriveNewFolderInsertDTO FolderNewInsert(DriveNewFolderInsertDTO driveNewFolderInsertDTO) { // 조건걸고 수정해야됨
+
+
+        String parentFolderPath = "/documents";
+
+        if(driveNewFolderInsertDTO.getDriveParentFolderId() != null) {
+           driveFolderRepository.findById(driveNewFolderInsertDTO.getDriveFolderId());
+        }else {
+            String newFolderPath = parentFolderPath + "/" + driveNewFolderInsertDTO.getDriveFolderName();
+            DriveFolderDocument newFolder = DriveFolderDocument.builder()
+                    .driveFolderName(driveNewFolderInsertDTO.getDriveFolderName())
+                    .driveFolderPath(newFolderPath)
+                    .driveFolderCreatedAt(LocalDateTime.now())
+                    .driveFolderUpdatedAt(LocalDateTime.now())
+                    .build();
+
+            return modelMapper.map(driveFolderRepository.save(newFolder), DriveNewFolderInsertDTO.class);
+
+        }
+    return null;
+    }
+    public List<MyDriveViewDTO> MyDriveView() {
+        List<DriveFolderDocument> MyDriveFolders = driveFolderRepository.findAllWithFolders();
+
+        List<MyDriveViewDTO> myDriveViewDTOList = MyDriveFolders.stream()
+                .map(folder -> modelMapper.map(folder, MyDriveViewDTO.class))
+                .collect(Collectors.toList());
+
+
+        return myDriveViewDTOList;
+    }
+}
