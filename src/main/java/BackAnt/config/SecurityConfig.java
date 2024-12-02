09c68c695898feccce943dd27d383a678f51262b
junhyeokkk <loveu9911@naver.com>
2024-12-02 17:43:19 +0900

    cors 도메인 설정

--- src/main/java/BackAnt/config/SecurityConfig.java
+++ src/main/java/BackAnt/config/SecurityConfig.java
@@ -68,9 +68,10 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(
-                "http://localhost:5174"
+                "http://localhost:5173",
+                "http://localhost:5174",
 //                "http://127.0.0.1:5174",
-//                "http://127.0.0.1:5173",
+                "http://3.34.40.220"
 //                "http://127.0.0.1:8080",
 //                "http://localhost:8080"
         ));
