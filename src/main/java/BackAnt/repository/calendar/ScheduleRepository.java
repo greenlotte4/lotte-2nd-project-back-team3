a53848f4dc42bd2409a62f146335dd2b299a9f09
gkdntn <gkdntn@gmail.com>
2024-12-03 17:56:10 +0900

    캘린더

---
+++ src/main/java/BackAnt/repository/calendar/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package BackAnt.repository.calendar;
+
+import BackAnt.entity.calendar.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+}
