b885de4885a5aeeab953e8b5a2e38e8f5c063326
junhyeokkk <loveu9911@naver.com>
2024-12-06 13:43:57 +0900

    근태수정

--- src/main/java/BackAnt/controller/user/AttendanceController.java
+++ src/main/java/BackAnt/controller/user/AttendanceController.java
@@ -1,10 +1,14 @@
 package BackAnt.controller.user;
 
+import BackAnt.entity.Attendance;
 import BackAnt.service.AttendanceService;
+import BackAnt.util.AttendanceFormatter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RestController
 @RequestMapping("/api/attendance")
 @RequiredArgsConstructor
@@ -12,12 +16,14 @@ public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
+
     // 출근 처리
     @PostMapping("/check-in/{userId}")
     public ResponseEntity<?> checkIn(@PathVariable Long userId) {
         try {
-            attendanceService.checkIn(userId);
-            return ResponseEntity.ok("출근이 성공적으로 처리되었습니다.");
+            Attendance attendance = attendanceService.checkIn(userId);
+            log.info("출근시간"+ String.valueOf(attendance.getCheckIn()));
+            return ResponseEntity.ok( AttendanceFormatter.formatCheckInTime(String.valueOf(attendance.getCheckIn()))); // 출근 기록 반환
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
@@ -29,8 +35,9 @@ public class AttendanceController {
     @PostMapping("/check-out/{userId}")
     public ResponseEntity<?> checkOut(@PathVariable Long userId) {
         try {
-            attendanceService.checkOut(userId);
-            return ResponseEntity.ok("퇴근이 성공적으로 처리되었습니다.");
+            Attendance attendance = attendanceService.checkOut(userId);
+            log.info("퇴근시간"+ String.valueOf(attendance.getCheckOut()));
+            return ResponseEntity.ok( AttendanceFormatter.formatCheckInTime(String.valueOf(attendance.getCheckOut())));// 퇴근 기록 반환
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
@@ -42,12 +49,13 @@ public class AttendanceController {
     @PutMapping("/update-status/{userId}")
     public ResponseEntity<?> updateStatus(@PathVariable Long userId, @RequestParam String status) {
         try {
-            attendanceService.updateStatus(userId, status);
-            return ResponseEntity.ok("상태가 성공적으로 업데이트되었습니다.");
+            Attendance attendance = attendanceService.updateStatus(userId, status);
+            return ResponseEntity.ok(attendance); // 업데이트된 기록 반환
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(500).body("서버 오류가 발생했습니다.");
         }
     }
+
 }
